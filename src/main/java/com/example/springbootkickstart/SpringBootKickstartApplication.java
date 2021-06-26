374a0a66555cdb80857b47dd17c05b414e6c6939
John Bai <pikapai@gmail.com>
2021-06-26 23:43:48 +0800

    Initial Commit close #1

---
+++ src/main/java/com/example/springbootkickstart/SpringBootKickstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKickstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootkickstart/SpringBootKickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
