8f0170a9f7fff7c64631d5d9cea9482dd6d00331
John Bai <pikapai@gmail.com>
2021-06-29 18:28:33 +0800

    Add Swagger API Doc support closes #5

---
+++ src/main/java/com/example/springbootkickstart/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.example.springbootkickstart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket buildDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInf())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.springbootkickstart.web.controllers"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInf() {
+        return new ApiInfoBuilder()
+                .title("Example API documentation")
+                .contact(new Contact("John Doe", "https://www.example.com", "john.doe@example.com"))
+                .version("1.0")
+                .build();
+    }
+}
